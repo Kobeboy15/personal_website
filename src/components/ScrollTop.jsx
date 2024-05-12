8d7fd003c6d7d4eff9a4aaba9c6b55ac1c59c48c
Kobe Michael <kobemichael15@gmail.com>
2024-05-12 10:59:52 -0700

    Remove old unused files

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,11 +1,32 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ScrollTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function checkScrollPosition() {
+    const button = document.getElementById("scroll-up-button");
+    if (window.scrollY === 0) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollPosition);
+  }, []);
+
   return (
-    <div className="px-24 fixed right-0 bottom-[30px]">
+    <div
+      id="scroll-up-button"
+      className={`px-24 fixed right-0 bottom-[30px] ${
+        isVisible ? "fade-in" : "fade-out"
+      }`}
+    >
       <button
+        disabled={!isVisible}
         onClick={() => {
           window.scrollTo({ top: 0, behavior: "smooth" });
         }}
